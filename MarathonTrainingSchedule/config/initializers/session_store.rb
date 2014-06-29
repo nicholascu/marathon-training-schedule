143f7c8af2b7c5c6013719f8a1e1570b1690a1f4
nicholascu <nicholascu@gmail.com>
2014-06-28 22:12:08 -0700

    Create skeleton for new rails app name MarathonTrainingSchedule

---
+++ MarathonTrainingSchedule/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MarathonTrainingSchedule::Application.config.session_store :cookie_store, key: '_MarathonTrainingSchedule_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MarathonTrainingSchedule::Application.config.session_store :active_record_store
