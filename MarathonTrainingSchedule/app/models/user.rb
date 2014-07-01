73ae56c8164881fb36af32e7d020e763398a3681
nicholascu <nicholascu@gmail.com>
2014-06-30 23:04:05 -0700

    Add before and filter to user method to encrypt password with BCrypt

--- MarathonTrainingSchedule/app/models/user.rb
+++ MarathonTrainingSchedule/app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :email, :presence => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  before_save :encrypt_password
+  after_save :clear_password
+
+  def encrypt_password
+  	if password.present?
+  		self.salt = BCrypt::Engine.generate_salt
+  		self.encrypted_passwords = BCrypt::Engine.hash_secret(password, salt)
+  	end
+  end
+  def clear_password
+  	self.password = nil
+  end
 end
