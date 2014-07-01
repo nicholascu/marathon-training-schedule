c0d9d4dc9e7f11283004cbf4dc606ba5013d1947
nicholascu <nicholascu@gmail.com>
2014-06-30 23:32:37 -0700

    Add login attempt action to Sessions Controller

--- MarathonTrainingSchedule/app/controllers/sessions_controller.rb
+++ MarathonTrainingSchedule/app/controllers/sessions_controller.rb
@@ -2,6 +2,18 @@ class SessionsController < ApplicationController
   def login
   end
 
+  def login_attempt
+  	authorized_user = User.authenticate(params[:username_or_email], params[:login_password])
+  	if authorized_user
+      flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
+      redirect_to(:action => 'home')
+    else
+      flash[:notice] = "Invalid Username or Password"
+      flash[:color]= "invalid"
+      render "login"	
+    end
+  end
+
   def home
   end
 
