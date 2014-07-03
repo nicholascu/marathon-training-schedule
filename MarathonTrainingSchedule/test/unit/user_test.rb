3683e29d8cfe54053667bdfa0cb3ba1aea472d20
nicholascu <nicholascu@gmail.com>
2014-07-03 00:13:23 -0700

    Add name validation unit test

--- MarathonTrainingSchedule/test/unit/user_test.rb
+++ MarathonTrainingSchedule/test/unit/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "invalid_without_a_name" do 
+  	z = User.new
+  	assert !z.valid?, "Name is not being validated"
+  end
 end
