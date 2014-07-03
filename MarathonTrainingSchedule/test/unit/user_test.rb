45a6497ddaaa9b066eabf0c37ee5fd59cca1f5a1
nicholascu <nicholascu@gmail.com>
2014-07-03 00:17:08 -0700

    Add fixture for unit test

--- MarathonTrainingSchedule/test/unit/user_test.rb
+++ MarathonTrainingSchedule/test/unit/user_test.rb
@@ -8,4 +8,12 @@ class UserTest < ActiveSupport::TestCase
   	z = User.new
   	assert !z.valid?, "Name is not being validated"
   end
+
+	test "valid with all attributes" do 
+		z = User.new
+		z.name = "Nic"
+		z.email = "nic@nic.com"
+		z.username = "nic"
+		assert z.valid?, "User was not valid"
+	end
 end
