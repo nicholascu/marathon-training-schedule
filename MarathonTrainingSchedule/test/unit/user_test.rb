32dd99e54523a81193da7c891b4d817a5fd7222b
nicholascu <nicholascu@gmail.com>
2014-07-03 01:15:30 -0700

    Add more unit test

--- MarathonTrainingSchedule/test/unit/user_test.rb
+++ MarathonTrainingSchedule/test/unit/user_test.rb
@@ -10,10 +10,19 @@ class UserTest < ActiveSupport::TestCase
   end
 
 	test "valid with all attributes" do 
-		z = User.new
-		z.name = "Nic"
-		z.email = "nic@nic.com"
-		z.username = "nic"
+		# z = User.new
+		# z.name = "Nic"
+		# z.email = "nic@nic.com"
+		# z.username = "nic"
+		z = users(:nic)
 		assert z.valid?, "User was not valid"
 	end
+
+test "invalid name gives error message" do
+	z = users(:nic)
+	z.name = nil
+	z.valid?
+	assert_match /can't be blank/, z.errors[:name].join
+end
+
 end
