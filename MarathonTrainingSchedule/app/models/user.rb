2e890ec464b88ae82d4e389924c52ad1efd8378f
nicholascu <nicholascu@gmail.com>
2014-07-05 01:11:00 -0700

    Add username field and self authenticate method in user model

--- MarathonTrainingSchedule/app/models/user.rb
+++ MarathonTrainingSchedule/app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password
+  attr_accessible :email, :name, :password, :username
+
+  def self.authenticate(username_or_email="", login_password="")
+  	if  EMAIL_REGEX.match(username_or_email)    
+  	  user = User.find_by_email(username_or_email)
+  	else
+  	  user = User.find_by_username(username_or_email)
+  	end
+  	if user && user.match_password(login_password)
+  	  return user
+  	else
+  	  return false
+  	end
+  end
 end
