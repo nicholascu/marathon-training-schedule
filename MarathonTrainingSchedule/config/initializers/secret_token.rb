6fb5183db21ff173a304c6cadba7dab4159f6aa8
nicholascu <nicholascu@gmail.com>
2014-07-04 22:36:26 -0700

    New App

---
+++ MarathonTrainingSchedule/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarathonTrainingSchedule::Application.config.secret_token = 'aff3858ccbc1de1e42913e46a628d8a93d4b37342297fe14fd1aa2324b9c3d92325be07bab8bd7dc2f43218405d8534cd9831e384e05e6becf6b6ea26306d309'
